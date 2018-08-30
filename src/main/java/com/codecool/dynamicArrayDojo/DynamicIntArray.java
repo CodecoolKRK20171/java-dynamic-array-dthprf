04c48d193bfa0786018f20fa21a459000a663041
Bart <bartpetka@gmail.com>
2018-08-30 16:11:44 +0200

    Add to git

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,6 +1,91 @@
 package com.codecool.dynamicArrayDojo;
 
-// put your code here!
-public class DynamicIntArray {
+import java.util.Arrays;
 
+public class DynamicIntArray {
+    private static final int INITIAL_CAPACITY = 10;
+    private int size = 0;
+    private int[] array = {};
+
+    public DynamicIntArray() {
+        this.array = new int[INITIAL_CAPACITY];
+    }
+
+    public DynamicIntArray(int length) {
+        this.array = new int[length];
+
+    }
+
+    public void add(int element) {
+        if (size == array.length) {
+            enlargeCapacity();
+        }
+        this.array[size] = element;
+        this.size++;
+    }
+
+    private void enlargeCapacity() {
+        int newIncreasedCapacity = array.length * 2;
+        array = Arrays.copyOf(array, newIncreasedCapacity);
+    }
+
+    public void remove(int index) {
+        if (index > size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException();
+
+        } else {
+
+            int[] arrayAfter = new int[this.size - 1];
+
+            int j = 0;
+
+            for (int i = 0; i < size; i++) {
+                if (i == index) {
+                    i++;
+                }
+
+                if (i == size) {
+                    break;
+                }
+
+                arrayAfter[j] = array[i];
+                j++;
+            }
+
+            this.array = arrayAfter;
+            size --;
+            }
+        }
+
+    public void insert(int index, int element) {
+        if (size == array.length) {
+            enlargeCapacity();
+        }
+
+        if (index > size) {
+            index = size;
+        }
+
+        int[] result = new int[size + 1];
+        System.arraycopy(array, 0, result, 0, index);
+        System.arraycopy(array, index, result, index + 1, size - index);
+        result[index] = element;
+        this.array = result;
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            sb.append(" ");
+            sb.append(array[i]);
+        }
+
+        return sb.toString();
+    }
+
+    public int length() {
+        return this.size;
+    }
 }
--- src/main/java/com/codecool/dynamicArrayDojo/Program.java
+++ src/main/java/com/codecool/dynamicArrayDojo/Program.java
@@ -1,8 +1,17 @@
 package com.codecool.dynamicArrayDojo;
 
+import java.util.ArrayList;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Do not run me, run the TESTS!");
+        DynamicIntArray array = new DynamicIntArray(10);
+        for (int i = 0; i < 11; ++i) {
+            array.add(i);
+        }
+
+        array.insert(8, 223);
+        array.insert(100, 654);
+        System.out.println(array);
     }
 }
